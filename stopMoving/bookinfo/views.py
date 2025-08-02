e45a8ba7ea71e7cd7fcb022f50569bf3e4614518
jjjaehong <woghd3014@gmail.com>
2025-08-02 23:46:10 +0900

    스웨거 설정 완료- 최종

--- stopMoving/bookinfo/views.py
+++ stopMoving/bookinfo/views.py
@@ -4,9 +4,20 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.conf import settings
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 # Create your views here.
 
 class BookLookUpAPIView(APIView):
+    @swagger_auto_schema(
+        operation_description="ISBN으로 도서 정보 조회",
+        manual_parameters=[
+            openapi.Parameter(
+                'isbn', openapi.IN_QUERY, description="ISBN 코드", type=openapi.TYPE_STRING, required=True
+            )
+        ],
+        responses={200: '성공', 400: '잘못된 요청', 502: '외부 API 오류'}
+    )
     def get(self, request):
         isbn = requests.query_params.get('isbn') # 프론트에서 넘겨주는 isbn
         if not isbn:
