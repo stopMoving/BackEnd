648e3dbc4bdb6a6d0de847908cb215a823f9a034
chaeyeonlee898 <chaeyeonlee898@gmail.com>
2025-08-17 18:31:33 +0900

    feat: 도서관 전체 목록 반환

--- stopMoving/library/urls.py
+++ stopMoving/library/urls.py
@@ -1,8 +1,9 @@
 # libraryapp/urls.py
 from django.urls import path
-from .views import LibraryDetailAPIView, LibraryBooksAPIView
+from .views import LibraryDetailAPIView, LibraryBooksAPIView, LibraryListAPIView
 
 urlpatterns = [
     path("detail/<int:library_id>/", LibraryDetailAPIView.as_view(), name="library-detail"), # 도서관 상세 정보 조회
     path('booklist/<int:library_id>/', LibraryBooksAPIView.as_view(), name='library-books'), # 도서관별 책 목록 조회
+    path('list/', LibraryListAPIView.as_view(), name="library-list") # 전체 도서관 목록 조회
 ]
