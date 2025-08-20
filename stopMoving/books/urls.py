736793e0ad291b26633d1d434718f5307eb5f6ee
jjjaehong <woghd3014@gmail.com>
2025-08-21 00:21:34 +0900

    feature:책 가져가기 시 isbn와 도서관 id 받고 책 정보 및 구매가능 book_id반환

--- stopMoving/books/urls.py
+++ stopMoving/books/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("pickup/", PickupAPIView.as_view(), name="books-pick"),
     path("donate/", DonationAPIView.as_view(), name="books-donate"),
     path("by-isbn/<str:isbn>/",BookDetailAPIView.as_view(), name="books-detail"),
-    path("by-id/<int:book_id>/",PickUpBookDetailAPIView.as_view(), name="pickUpBooks-detail"),
+    path("pickup/detail/",PickUpBookDetailAPIView.as_view(), name="pickUpBooks-detail"),
 ]
