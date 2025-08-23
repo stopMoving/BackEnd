ad795addb9ad7497f020efeccd11931d280a52ad
chaeyeonlee898 <chaeyeonlee898@gmail.com>
2025-08-23 20:43:44 +0900

    feat: 도서관별 보유 도서 안에서 사용자 맞춤 추천 api

--- stopMoving/library/urls.py
+++ stopMoving/library/urls.py
@@ -1,10 +1,11 @@
 # libraryapp/urls.py
 from django.urls import path
-from .views import LibraryDetailAPIView, LibraryBooksAPIView, LibraryListAPIView, LibraryImageUploadView
+from .views import LibraryDetailAPIView, LibraryBooksAPIView, LibraryListAPIView, LibraryImageUploadView, LibraryRecommendationView
 
 urlpatterns = [
     path("detail/<int:library_id>/", LibraryDetailAPIView.as_view(), name="library-detail"), # 도서관 상세 정보 조회
     path('booklist/<int:library_id>/', LibraryBooksAPIView.as_view(), name='library-books'), # 도서관별 책 목록 조회
     path('list/', LibraryListAPIView.as_view(), name="library-list"), # 전체 도서관 목록 조회
-    path('upload/', LibraryImageUploadView.as_view(), name='image-upload') # 도서관 이미지 업로드 
+    path('upload/', LibraryImageUploadView.as_view(), name='image-upload'), # 도서관 이미지 업로드 
+    path('recommendations/<int:library_id>/', LibraryRecommendationView.as_view(), name='library-recommendation' ) # 도서관 보유 도서에서 책 추천
 ]
