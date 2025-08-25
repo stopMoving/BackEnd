eb1f87da93f8f1739d4392abdb5632fac038625c
jjjaehong <woghd3014@gmail.com>
2025-08-25 18:15:32 +0900

    fix:액세스 토큰 수정

--- stopMoving/config/settings.py
+++ stopMoving/config/settings.py
@@ -229,7 +229,7 @@ REST_FRAMEWORK = {
 REST_USE_JWT = True
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=20),    # 유효기간 20일 -> 배포시 3시간으로 변경
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),    # 유효기간 20일 -> 배포시 3시간으로 변경
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),    # 유효기간 7일
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
